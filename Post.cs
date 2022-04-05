03a84b0b6bd67916e01d0e1a5e88e9ff3fe4dda8
Александр Колодинский <photon.74@gmail.com>
2022-04-05 21:47:55 +0300

    1

---
+++ Post.cs
@@ -0,0 +1,15 @@
+﻿namespace Posts
+{
+    internal class Post
+    {
+        public int UserId { get; set; }
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Body { get; set; }
+
+        public override string ToString()
+        {
+            return $"UserId: {UserId}\nId: {Id}\nTitle: {Title}\nBody: {Body}\n";
+        }
+    }
+}
